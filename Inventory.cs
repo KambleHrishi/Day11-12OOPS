96bd4f0fff255d8ff2c9f8f6cf4602dec851e1e2
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-27 23:10:46 +0530

    Inventory for rices pulses wheats

---
+++ Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11OOPS
+{
+    public class Inventory
+    {
+        public List<TypesofRice> typesofRice;
+        public List<TypesofPulses> typesofPulses;
+        public List<TypesofWheats> typesofWheats;
+    }
+    public class TypesofRice
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+    }
+    public class TypesofPulses
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+    }
+    public class TypesofWheats
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,9 +2,51 @@
 {
     class Program
     {
-        public static void Main(string[]args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Day 11-12 OOPS problems");
+            string path = "C:/Users/kambl/OneDrive/Desktop/RFP208/Day11OOPS/Inventory.json";
+            FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
+            Inventory data = fetchInventoryDetails.Read(path);
+            Console.WriteLine("---------------");
+            Console.WriteLine("Inventory Details");
+            Console.WriteLine("--------------");
+
+            Console.WriteLine("Types Of Rice");
+
+            for (int i = 0; i < data.typesofRice.Count; i++)
+            {
+                //Console.WriteLine(data.typesofRice[i].name);
+                //Console.WriteLine(data.typesofRice[i].weight);
+                //Console.WriteLine(data.typesofRice[i].price);
+                int val = data.typesofRice[i].weight * data.typesofRice[i].price;
+                Console.WriteLine("The Price for {0}kg of type {1} is {2} ", data.typesofRice[i].weight, data.typesofRice[i].price, val);
+                Console.WriteLine("--------------------------------------");
+            }
+            Console.WriteLine("--------------");
+            Console.WriteLine("Types Of Pulses");
+
+            for (int i = 0; i < data.typesofPulses.Count; i++)
+            {
+                //Console.WriteLine(data.typesofPulses[i].name);
+                //Console.WriteLine(data.typesofPulses[i].weight);
+                //Console.WriteLine(data.typesofPulses[i].price);
+                int val = data.typesofPulses[i].weight * data.typesofPulses[i].price;
+                Console.WriteLine("The Price for {0}kg of type {1} is {2} ", data.typesofPulses[i].weight, data.typesofPulses[i].price, val);
+                Console.WriteLine("--------------------------------------");
+            }
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Types Of Wheats");
+
+            for (int i = 0; i < data.typesofWheats.Count; i++)
+            {
+                //Console.WriteLine(data.typesofWheats[i].name);
+                //Console.WriteLine(data.typesofWheats[i].weight);
+                //Console.WriteLine(data.typesofWheats[i].price);
+                int val = data.typesofWheats[i].weight * data.typesofWheats[i].price;
+                Console.WriteLine("The Price for {0}kg of type {1} is {2} ", data.typesofWheats[i].weight, data.typesofWheats[i].price, val);
+                Console.WriteLine("---------------------------------------");
+            }
+            Console.WriteLine("----------------");
         }
     }
 }
