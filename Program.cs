409adecf4b39008c452968ff7fc894e28b33903f
hrishikesh kamble <hrishikamble13@gmail.com>
2022-10-27 22:55:14 +0530

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day11OOPS
+{
+    class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome To Day 11-12 OOPS problems");
+        }
+    }
+}
